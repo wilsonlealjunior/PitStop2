9956fc7ebe88f83e0ba69d0a94076936e97cbdde
wilsonlealjunior@hotmail.com <88231845w>
2018-05-11 13:41:19 -0300

    novos icones

---
+++ app/src/main/java/pitstop/com/br/pitstop/Application.java
@@ -0,0 +1,36 @@
+package pitstop.com.br.pitstop;
+
+import android.util.Log;
+
+import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+/**
+ * Created by wilso on 04/05/2018.
+ */
+
+public class Application extends android.app.Application {
+
+
+    public Application() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder()
+                .deleteRealmIfMigrationNeeded()
+                .name("PitStop.realm")
+                .build();
+        Realm.setDefaultConfiguration(realmConfig);
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                        .build());
+    }
+}
